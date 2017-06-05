dfd5c3cacc998029831a29d2cc122cad580947b1
arinovich.anatoli <arinov@tut.by>
2017-06-05 18:12:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -20,42 +20,44 @@ var base = []; // todo массив ссылок на полный список 
 
 
 // todo получение списка всех ссылок на все марки
-var baseURI = 'https://www.abw.by/'
+var baseURI = 'https://www.abw.by/';
+for ( var index =0; index<URL.length ; index++){
+    var q = tress(function(url, callback){
+        needle.get(url, function(err, res){
+            if (err) throw err;
 
-var q = tress(function(url, callback){
-    needle.get(url, function(err, res){
-        if (err) throw err;
-
-        var $ = cheerio.load(res.body);
-        //Todo абирает список марок с домашней страницы ABW.by
-        $('ul.filter-marka > li.filter-marka-item').each(function (i) {
-            base.push({
-                name: $('ul.filter-marka > li.filter-marka-item a').eq(i).text().trim(),
-                value: $('ul.filter-marka > li.filter-marka-item a > span').eq(i).text(), // может понадобится для определения изменений в списке (количество записей по марке)
-                link: $('ul.filter-marka > li.filter-marka-item a').eq(i).attr('href')
+            var $ = cheerio.load(res.body);
+            //Todo абирает список марок с домашней страницы ABW.by
+            $('ul.filter-marka > li.filter-marka-item').each(function (i) {
+                base.push({
+                    name: $('ul.filter-marka > li.filter-marka-item a').eq(i).text().trim(),
+                    value: $('ul.filter-marka > li.filter-marka-item a > span').eq(i).text(), // может понадобится для определения изменений в списке (количество записей по марке)
+                    link: $('ul.filter-marka > li.filter-marka-item a').eq(i).attr('href')
+                });
             });
+            //Todo абирает список марок с домашней страницы AV.by
+            // $('ul.brandslist > li.brandsitem.brandsitem--primary').each(function (i) {
+            //     base.push({
+            //         name: $('li.brandsitem.brandsitem--primary span').eq(i).text(),
+            //         value: $('li.brandsitem.brandsitem--primary small').eq(i).text(), // может понадобится для определения изменений в списке (количество записей по марке)
+            //         link: $('li.brandsitem.brandsitem--primary a').eq(i).attr('href')
+            //     });
+            //     AV(base[i].link);
+            // });
+
+            callback();
         });
-        //Todo абирает список марок с домашней страницы AV.by
-        // $('ul.brandslist > li.brandsitem.brandsitem--primary').each(function (i) {
-        //     base.push({
-        //         name: $('li.brandsitem.brandsitem--primary span').eq(i).text(),
-        //         value: $('li.brandsitem.brandsitem--primary small').eq(i).text(), // может понадобится для определения изменений в списке (количество записей по марке)
-        //         link: $('li.brandsitem.brandsitem--primary a').eq(i).attr('href')
-        //     });
-        //     AV(base[i].link);
-        // });
+    }, 1);
 
-        callback();
-    });
-}, 1);
-
-q.drain = function(){
-    fs.writeFileSync('./abw/'+URL[1].name+'.json', JSON.stringify(base, null, 4));
-    var sch_link = [];
-    for( i=0; i<base.length; i++) {
-        sch_link.push(AV(base[i].link));
+    q.drain = function(){
+        fs.writeFileSync('./abw/'+URL[index-1].name+'.json', JSON.stringify(base, null, 4));
+        var sch_link = [];
+        for( i=0; i<base.length; i++) {
+            sch_link.push(AV(base[i].link));
+        }
+        console.log(sch_link);
     }
-    console.log(sch_link);
+
+    q.push(URL[index].URL);
 }
 
-q.push(URL[1].URL);
