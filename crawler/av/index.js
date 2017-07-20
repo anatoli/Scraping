8f6adb251054463dc109a49609f598933e5c7b1a
arinovich.anatoli <arinov@tut.by>
2017-07-20 18:19:28 +0300

    Скраббер для выкачки моделей брендов и моделей для AV.

--- crawler/av/index.js
+++ crawler/av/index.js
@@ -4,7 +4,7 @@ var needle = require('needle');
 var cheerio = require('cheerio');
 var resolve = require('url').resolve;
 var fs = require('fs');
-var request = require('request-promise');
+var request = require('request-sync');
 var module_detail = require('./module_detail');
 
 var URL = {
@@ -28,36 +28,34 @@ var AV = module.exports = function () { // brand, model, year_from, year_to,pric
             var $ = cheerio.load(res.body);
             var options = $('select[name *= "brand_id"] option');
             options.each(function (each_index) {
+                console.log('index = ' + each_index);
                 var id = $(this).attr('value');
                 var name = $(this).text();
                 var model_array =  request({
                     method: 'GET',
                     uri: "https://av.by/public/parameters.php?event=Model_List_Search&category_parent="+id
-                })
-                    .then(function (res, body) {
-                        var models_str_array = res.split('\n');
-                        var car_models = []; //todo список моделей полученный динамически.  массив для распршивания строки моделей.
-                        models_str_array.forEach( function (el, index, p3) {
-                            var i = el.indexOf('=');
-                            if (el.substring(0,i) != 0) {
-                                car_models.push({
-                                    id: el.substring(0,i),
-                                    name: el.substring(i+1)
-                                });
-                            }
-                        })
-                        models.push({
-                            id: id,
-                            name: name,
-                            model: car_models
+                });
+
+                var models_str_array = model_array.body.split('\n');
+                var car_models = []; //todo список моделей полученный динамически.  массив для распршивания строки моделей.
+                models_str_array.forEach( function (el, index, p3) {
+                    var i = el.indexOf('=');
+                    if (el.substring(0,i) != 0) {
+                        car_models.push({
+                            id: el.substring(0,i),
+                            name: el.substring(i+1)
                         });
-                        if(each_index === options.length-2 ) {
-                            fs.appendFileSync('./av/cars_model_id.json', JSON.stringify(models, null, 4));
-                        }
-                    })
-                    .catch(function (err) {
-                        // console.log(err)
-                    })
+                    }
+                })
+                models.push({
+                    id: id,
+                    name: name,
+                    model: car_models
+                });
+                if(each_index === options.length-1 ) {
+                    fs.writeFileSync('./av/cars_model_id.json', JSON.stringify(models, null, 4));
+
+                }
                 if (id !== "") {
                     brand.push({
                         id: id,
