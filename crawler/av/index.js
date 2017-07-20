e364a5e2dacc165c8ff15001e21bbe596ea367ba
arinovich.anatoli <arinov@tut.by>
2017-07-20 15:26:11 +0300

    Добавлена логика забора списка брендов, примерно 96, со списком моделей.

--- crawler/av/index.js
+++ crawler/av/index.js
@@ -16,24 +16,9 @@ var brand = []; // todo массив ссылок на полный список
 
 var AV = module.exports = function () { // brand, model, year_from, year_to,price_from, price_to
 
-
-    function modelsBrands(id) { // возвращает список моделей для id - бренда.
-
-        const options = {
-            method: 'GET',
-            uri: "https://av.by/public/parameters.php?event=Model_List_Search&category_parent="+id }
-​
-        request(options)
-            .then(function (res) {
-                return res.data;
-            })
-            .catch(function (err) {
-               console.log(err)
-            })
-    }
-
 // todo получение списка всех ссылок на все марки
     var q = tress(function (url, callback) {
+        var models= [];
         needle.get(url, function (err, res) {
             if (err || res.statusCode !== 200) {
                 log.e((err || res.statusCode) + ' - ' + url);
@@ -41,18 +26,48 @@ var AV = module.exports = function () { // brand, model, year_from, year_to,pric
             }
 
             var $ = cheerio.load(res.body);
-            $('select[name *= "brand_id"] option').each(function () {
+            var options = $('select[name *= "brand_id"] option');
+            options.each(function (each_index) {
                 var id = $(this).attr('value');
                 var name = $(this).text();
-                brand.push({
-                    id: id,
-                    name: name,
-                    model_array: modelsBrands(id)
-
+                var model_array =  request({
+                    method: 'GET',
+                    uri: "https://av.by/public/parameters.php?event=Model_List_Search&category_parent="+id
                 })
-            })
-            console.log(res.body);
-            console.log(brand);
+                    .then(function (res, body) {
+                        var models_str_array = res.split('\n');
+                        var car_models = []; //todo список моделей полученный динамически.  массив для распршивания строки моделей.
+                        models_str_array.forEach( function (el, index, p3) {
+                            var i = el.indexOf('=');
+                            if (el.substring(0,i) != 0) {
+                                car_models.push({
+                                    id: el.substring(0,i),
+                                    name: el.substring(i+1)
+                                });
+                            }
+                        })
+                        models.push({
+                            id: id,
+                            name: name,
+                            model: car_models
+                        });
+                        if(each_index === options.length-2 ) {
+                            fs.appendFileSync('./av/cars_model_id.json', JSON.stringify(models, null, 4));
+                        }
+                    })
+                    .catch(function (err) {
+                        // console.log(err)
+                    })
+                if (id !== "") {
+                    brand.push({
+                        id: id,
+                        name: name,
+                        model_array: model_array
+
+                    })
+                }
+            });
+            callback();
         });
     }, -1000);
 
